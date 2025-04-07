e435eecbd939031419f55358ab316dccd71d78d9
Shabib145 <sadidurrahmanshabib1459@gmail.com>
2025-04-08 00:24:56 +0600

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,86 @@
+
+ function btn7(){
+   const display=document.getElementById("input");
+   display.value+="7";
+ }
+ function btn8(){
+   const display=document.getElementById("input");
+   display.value+="8";
+ }
+ function btn9(){
+   const display=document.getElementById("input");
+   display.value+="9";
+ }
+ function btn1(){
+   const display=document.getElementById("input");
+   display.value+="1";
+ }
+ function btn2(){
+   const display=document.getElementById("input");
+   display.value+="2";
+ }
+ function btn3(){
+   const display=document.getElementById("input");
+   display.value+="3";
+ }
+ function btn4(){
+   const display=document.getElementById("input");
+   display.value+="4";
+ }
+ function btn5(){
+   const display=document.getElementById("input");
+   display.value+="5";
+ }
+ function btn6(){
+   const display=document.getElementById("input");
+   display.value+="6";
+ }
+ function btn6(){
+   const display=document.getElementById("input");
+   display.value+="6";
+ }
+ function btna(){
+   const display=document.getElementById("input");
+   display.value+="+";
+ }
+ function btna(){
+   const display=document.getElementById("input");
+   display.value+="+";
+ }
+ function btnac(){
+   const display=document.getElementById("input");
+   display.value="";
+ }
+ function btndel(){
+   const display=document.getElementById("input");
+   display.value= display.value.slice(0,-1);
+ }
+ function btne(){
+   const display=document.getElementById("input");
+   display.value=eval(display.value)
+ }
+ function btn0(){
+   const display=document.getElementById("input");
+   display.value+="0";
+ }
+ function btnd(){
+   const display=document.getElementById("input");
+   display.value+="/";
+ }
+ function btnm(){
+   const display=document.getElementById("input");
+   display.value+="*";
+ }
+ function btns(){
+   const display=document.getElementById("input");
+   display.value+="-";
+ }
+ function btnp(){
+  const display=document.getElementById("input");
+  display.value+=".";
+}
+
+
+
+
+  
